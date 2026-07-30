eb4d989da4e581e3de34f006d36f5e86b4dbe402
mattr13000 <mattjeux13@gmail.com>
2026-07-30 15:38:40 +0200

    mockup data for messages

---
+++ CraftProjectCDA/CraftProjectCDA/Data/ReviewMessages.swift
@@ -0,0 +1,20 @@
+//
+//  Untitled.swift
+//  CraftProjectCDA
+//
+//  Created by Apprenant 77 on 28/07/2026.
+//
+
+import Foundation
+
+var reviewMessages: [ReviewMessage] = [
+    ReviewMessage(content: "", senderID: user[0].id, receiverID: user[1].id, postDate: Date.now, rating: 4),
+    ReviewMessage(content: "", senderID: user[0].id, receiverID: user[0].id, postDate: Date.now, rating: 4),
+    ReviewMessage(content: "", senderID: user[0].id, receiverID: user[0].id, postDate: Date.now, rating: 4),
+    ReviewMessage(content: "", senderID: user[0].id, receiverID: user[0].id, postDate: Date.now, rating: 4),
+    ReviewMessage(content: "", senderID: user[0].id, receiverID: user[0].id, postDate: Date.now, rating: 4),
+    ReviewMessage(content: "", senderID: user[0].id, receiverID: user[0].id, postDate: Date.now, rating: 4),
+    ReviewMessage(content: "", senderID: user[0].id, receiverID: user[0].id, postDate: Date.now, rating: 4),
+    ReviewMessage(content: "", senderID: user[0].id, receiverID: user[0].id, postDate: Date.now, rating: 4),
+    ReviewMessage(content: "", senderID: user[0].id, receiverID: user[0].id, postDate: Date.now, rating: 4)
+]
