86916f50c61acc820a660f483df436501255be9a
mattr13000 <mattjeux13@gmail.com>
2026-07-31 16:36:36 +0200

    view foundations

---
+++ CraftProjectCDA/CraftProjectCDA/View/Components/DiscoveryHScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  DiscoveryHScrollView.swift
+//  CraftProjectCDA
+//
+//  Created by Apprenant 77 on 31/07/2026.
+//
+
+import SwiftUI
+
+struct DiscoveryHScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiscoveryHScrollView()
+}
