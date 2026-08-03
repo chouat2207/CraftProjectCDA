e725789675131fd0d3bb52ec08634f939d661cf8
Su <268640603+Su-marfr@users.noreply.github.com>
2026-08-03 14:15:32 +0200

    worked on artworkDetailView

---
+++ CraftProjectCDA/CraftProjectCDA/Views/ArtworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ArtworkDetailView.swift
+//  CraftProjectCDA
+//
+//  Created by Apprenant 89 on 31/07/2026.
+//
+
+import SwiftUI
+
+struct ArtworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtworkDetailView()
+}
