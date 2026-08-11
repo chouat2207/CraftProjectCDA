dc5ae4f0bcd2c7e68bcd9316530a761c610a5e50
djeska3 <djeska3@gmail.com>
2026-08-11 09:56:33 +0200

    ReviewsView

---
+++ CraftProjectCDA/CraftProjectCDA/View/Reviews/Components/MainNoteCard.swift
@@ -0,0 +1,33 @@
+//
+//  MainNoteCard.swift
+//  CraftProjectCDA
+//
+//  Created by Apprenant 85 on 10/08/2026.
+//
+
+import SwiftUI
+
+struct MainNoteCard: View {
+    var body: some View {
+       
+        
+        ZStack{
+            Image("ShapeReviews")
+                .resizable()
+                .scaledToFit()
+            Text("4,5")
+                .font(.system(size: 80))
+                .fontWeight(.bold)
+                .fontDesign(.rounded)
+                .foregroundStyle(.white)
+            
+        }
+        .frame(width: 270)
+       
+
+    }
+}
+
+#Preview {
+    MainNoteCard()
+}
---
+++ CraftProjectCDA/CraftProjectCDA/View/Reviews/Components/ReviewUserCard.swift
@@ -0,0 +1,76 @@
+//
+//  ReviewUserCard.swift
+//  CraftProjectCDA
+//
+//  Created by Apprenant 85 on 10/08/2026.
+//
+
+import SwiftUI
+
+struct ReviewUserCard: View {
+    @State var profileViewModel: ProfileViewModel = ProfileViewModel()
+   
+    var body: some View {
+        
+        
+        ZStack{
+            RoundedRectangle(cornerRadius: 30)
+                .frame(maxWidth: .infinity, minHeight: 120, maxHeight: 120)
+                .foregroundStyle(.gray.quinary)
+            
+           
+                HStack{
+                    Image(profileViewModel.mainUser.imageName)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 80, height: 80)
+                        .clipShape(Circle())
+                        .overlay(Circle()
+                            .stroke(.white, lineWidth: 5)
+                        )
+                        .padding(.bottom,45)
+                    Text(profileViewModel.mainUser.pseudonym)
+                        .font(.headline)
+                        .padding(.bottom,80)
+                    
+                    Spacer()
+                    
+                    HStack(spacing: 0.3){
+                        Image(systemName: "star.fill")
+                            .foregroundStyle(.yellow)
+                            .font(.title3)
+                        Image(systemName: "star.fill")
+                            .foregroundStyle(.yellow)
+                            .font(.title3)
+                        Image(systemName: "star.fill")
+                            .foregroundStyle(.yellow)
+                            .font(.title3)
+                        Image(systemName: "star.fill")
+                            .foregroundStyle(.yellow)
+                            .font(.title3)
+                        Image(systemName: "star.leadinghalf.filled")
+                            .foregroundStyle(.yellow)
+                            .font(.title3)
+                    }
+                    .padding()
+                    .padding(.bottom,80)
+                   
+                }
+            VStack{
+                Text("Un savoir-faire impressionnant, ainsi qu'une personnalité chaleureuse !")
+                    .frame(width: 290, height: 80)
+                    
+                    
+                    
+            }
+            .padding(.leading, 50)
+            .padding(.top,30)
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ReviewUserCard()
+}
