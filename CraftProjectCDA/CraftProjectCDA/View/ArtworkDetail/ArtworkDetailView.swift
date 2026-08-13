f5e22f6fb1379109e4fb3acade07565901724b10
Su <268640603+Su-marfr@users.noreply.github.com>
2026-08-13 14:03:06 +0200

    worked on artisan profile navigation

--- CraftProjectCDA/CraftProjectCDA/View/ArtworkDetail/ArtworkDetailView.swift
+++ CraftProjectCDA/CraftProjectCDA/View/ArtworkDetail/ArtworkDetailView.swift
@@ -13,99 +13,111 @@ struct ArtworkDetailView: View {
     let artwork: Artwork
     
     var body: some View {
- 
-            VStack{
-                ZStack(alignment: .bottomLeading){
-                    Image(viewModel.artwork.imageName)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(height: 280)
-                        .frame(maxWidth: 420)
-                        .clipped()
-                    
-                    HStack(){
-                        Image(viewModel.artisan.imageName)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100)
-                            .clipShape(Circle())
-                            .overlay(Circle()
-                                .stroke(Color.white, lineWidth: 5))
-                        
-                        VStack (alignment: .leading){
-                            Text(viewModel.artwork.name)
-                                .fontWeight(.semibold)
-                                .font(.title)
-                                .lineLimit(1)
-                                .padding(.top)
-                            
-                            Text("Par \(viewModel.artisan.firstName) \(viewModel.artisan.lastName) ")
+        
+        VStack{
+            ZStack(alignment: .bottomLeading){
+                Image(viewModel.artwork.imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 280)
+                    .frame(maxWidth: 420)
+                    .clipped()
+                
+                HStack {
+                    NavigationLink {
+                        if viewModel.artisan.artisanProfileID != nil {
+                            ArtisanProfileView(user: viewModel.artisan)
                         }
-                        Spacer()
-                        Button(action: {
-                            sharedVM.toggleFavorite(artworkID: viewModel.artwork.id)
-                        }) {
-                            Image(systemName: sharedVM.isFavorite(artworkID: viewModel.artwork.id) ? "heart.fill" : "heart")
-                                .font(.largeTitle)
-                                .foregroundStyle(.red)
-                        }
-                    }
-                    .alignmentGuide(.bottom, computeValue: { dimension in
-                        35
-                    })
-                    .padding(.horizontal,8)
-                }
-                ScrollView(.vertical/*, showsIndicators: false*/) {
-                    VStack(spacing: 10){
-                        Text("\(viewModel.artwork.description)")
-                            .padding(.bottom)
+//                        else {
+//                            ProfileView(user: viewModel.artisan)
+//                        }
+                    } label: {
                         
-                        Divider().background(.gray)
-                        HStack(/*alignment: .lastTextBaseline*/){
-                            Text("\(viewModel.artwork.details)")
-                            Image(viewModel.artwork.imageName)
+                        HStack(){
+                            Image(viewModel.artisan.imageName)
                                 .resizable()
-                                .scaledToFill()
+                                .scaledToFit()
+                                .frame(width: 100)
                                 .clipShape(Circle())
+                                .overlay(Circle()
+                                    .stroke(Color.white, lineWidth: 5))
+                            
+                            VStack (alignment: .leading){
+                                Text(viewModel.artwork.name)
+                                    .fontWeight(.semibold)
+                                    .font(.title)
+                                    .lineLimit(1)
+                                    .padding(.top)
+                                
+                                Text("Par \(viewModel.artisan.firstName) \(viewModel.artisan.lastName) ")
+                            }
                         }
-                        .padding(.top)
                     }
+                    Spacer()
+                    Button(action: {
+                        sharedVM.toggleFavorite(artworkID: viewModel.artwork.id)
+                    }) {
+                        Image(systemName: sharedVM.isFavorite(artworkID: viewModel.artwork.id) ? "heart.fill" : "heart")
+                            .font(.largeTitle)
+                            .foregroundStyle(.red)
+                    }
+                }
+                .alignmentGuide(.bottom, computeValue: { dimension in
+                    35
+                })
+                .padding(.horizontal,8)
+            }
+            ScrollView(.vertical/*, showsIndicators: false*/) {
+                VStack(spacing: 10){
+                    Text("\(viewModel.artwork.description)")
+                        .padding(.bottom)
+                    
+                    Divider().background(.gray)
+                    HStack(/*alignment: .lastTextBaseline*/){
+                        Text("\(viewModel.artwork.details)")
+                        Image(viewModel.artwork.imageName)
+                            .resizable()
+                            .scaledToFill()
+                            .clipShape(Circle())
+                    }
+                    .padding(.top)
+                }
+                .padding()
+            }
+            NavigationLink{
+                DirectMessageView(
+                    peerID: viewModel.artisan.id,
+                    peerName: viewModel.artisan.name,
+                    profileImageName: viewModel.artisan.imageName
+                )
+            }
+            label: {
+                Text("Contacter")
+                    .font(.body)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                    .frame(maxWidth: .infinity)
                     .padding()
-                }
-                NavigationLink{
-                    DirectMessageView(
-                        peerID: viewModel.artisan.id,
-                        peerName: viewModel.artisan.name,
-                        profileImageName: viewModel.artisan.imageName
-                    )
-                }
-                label: {
-                    Text("Contacter")
-                        .font(.body)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.black)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color(red: 0.4, green: 0.85, blue: 0.6))
-                        .cornerRadius(12)
-                }
-                
-                .padding(.horizontal)
-                //
-                //                .navigationDestination(isPresented: $viewModel.navigateToMessage) {
-                //                    DirectMessageView(
-                //                        peerID: viewModel.artisan.id,
-                //                        peerName: viewModel.artisan.name,
-                //                        profileImageName: viewModel.artisan.imageName
-                //                    )
-                //                }
-                
+                    .background(Color(red: 0.4, green: 0.85, blue: 0.6))
+                    .cornerRadius(12)
             }
             
-            .onAppear {
-                viewModel.artwork = artwork
-            }
+            .padding(.horizontal)
+            //
+            //                .navigationDestination(isPresented: $viewModel.navigateToMessage) {
+            //                    DirectMessageView(
+            //                        peerID: viewModel.artisan.id,
+            //                        peerName: viewModel.artisan.name,
+            //                        profileImageName: viewModel.artisan.imageName
+            //                    )
+            //                }
+            
         }
+        
+        .onAppear {
+            viewModel.artwork = artwork
+        }
+    }
 }
 
 #Preview {
