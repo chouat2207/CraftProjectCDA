6844b4bc49d4b798c4c1d44e1423f9ead2a87e11
Awatef <awatefchouat2207@gmail.com>
2026-08-13 15:36:43 +0200

    version finale carte liste

--- CraftProjectCDA/CraftProjectCDA/View/CarteView/CarteView.swift
+++ CraftProjectCDA/CraftProjectCDA/View/CarteView/CarteView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 import MapKit
 
 struct CarteView: View {
+    @Environment(SharedViewModel.self)
+    private var svm
+    
     let displayedArtisans: [ArtisanProfile]
     let hasActiveFilter: Bool
+    @State private var selectedArtisan: ArtisanProfile?
 
     @State private var position: MapCameraPosition = .userLocation(
        // true  → suit position + direction du téléphone
@@ -25,23 +29,38 @@ struct CarteView: View {
             UserAnnotation()
             ForEach(displayedArtisans) { artisan in
                 let coordinate = artisan.shopLocation
+                
                 let imageName = artisan.imageName
                 Annotation(
-                    "",
-                    coordinate: coordinate
-                ) {
-
-                    ArtisanMapAnnotation(
-                        imageName: imageName
-                    )
-                }
-            }
+                    "", coordinate: coordinate) {
+                    Button { selectedArtisan = artisan} 
+                    label: {
+                           ArtisanMapAnnotation(
+                            imageName: imageName)}
+                    .buttonStyle(.plain)
+                    
+                }}
         }
         .ignoresSafeArea()
         .onChange(of: displayedArtisans.map { $0.id }) {
             updateMapPosition()
         }
-    }
+        
+        .sheet(item: $selectedArtisan) { artisan in
+                Group {
+                    if let user = svm.usersData.first(
+                        where: { user in
+                        user.id == artisan.id
+                        })
+                    {
+                    ArtisanProfileView(user: user)
+                    } else {
+                        Text("Utilisateur introuvable")}
+                }
+                .presentationDetents([.medium,.large])
+                .presentationDragIndicator(.visible)
+            }
+        }
 
     private func updateMapPosition() {
         if hasActiveFilter {
@@ -56,4 +75,5 @@ struct CarteView: View {
 }
 #Preview {
     CarteView(displayedArtisans: artisanProfiles, hasActiveFilter: false)
+        .environment(SharedViewModel())
 }
